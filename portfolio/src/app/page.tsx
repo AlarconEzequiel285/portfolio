142090f8db0dff3fcb78d55d04113bec79faf6cd
Ezequiel Alarcon <alarcon.ezequiel285@gmail.com>
2025-07-28 19:57:14 -0300

    Tech-stack section added

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { FaLinkedinIn, FaGithub, FaXTwitter } from "react-icons/fa6";
+import { SiGithub, SiJavascript, SiMariadb, SiNextdotjs, SiNodedotjs, SiReact, SiTypescript } from "react-icons/si";
+import { TbBrandFramerMotion } from "react-icons/tb";
 
 export default function Home() {
   return (
@@ -50,7 +52,6 @@ export default function Home() {
                     university programmer, full stack developer
                   </h2>
                 </div>
-
                 {/* Redes Sociales / Contact me */}
                 <div className="ml-8">
                   <div className="flex gap-1">
@@ -66,12 +67,71 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-
               {/* Descripción debajo */}
               <div className="relative overflow-hidden">
                 <p className="text-lg leading-relaxed animate-text-sweep">
                  I build interactive web applications using modern technologies, such as React, Next.js, Tailwind CSS, and TypeScript. I'm very entusiastic about exploring creativity and innovation, always willing to learn new stuff. Driven by a desire to push the boundaries of web interaction.
                 </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Tech-Stack */}
+          <div className="mt-16">
+            <div className="flex items-start justify-center">
+              <div className="max-w-3xl w-full">
+                <div className="flex items-center justify-between mb-8">
+                  {/* Título principal */}
+                  <div className="flex-1">
+                    <div className="flex items-center gap-2">
+                      <h1 className="text-3xl text-white animate-color-sweep">
+                        tech-stack
+                      </h1>
+                      <h2 className="text-gray-300 text-sm relative top-[4px]">
+                       - learning and working with
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+                {/* Contenido de Tech-Stack */}
+                <div className="flex flex-col gap-3">
+                  <div className="flex gap-3">
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <SiReact className="text-cyan-400 text-xl" />
+                      React
+                    </div>
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <SiNodedotjs className="text-green-400 text-xl" />
+                      Node.js
+                    </div>
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <SiTypescript className="text-blue-400 text-xl" />
+                      TypeScript.js
+                    </div>
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <SiJavascript className="text-yellow-400 text-xl" />
+                      JavaScript.js
+                    </div>
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <SiMariadb className="text-blue-400 text-xl" />
+                      MariaDB
+                    </div>
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <SiNextdotjs className="text-black-400 text-xl" />
+                      Next.js
+                    </div>
+                  </div>
+                  <div className="flex gap-3">
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <SiGithub className="text-white-400 text-xl" />
+                      Git
+                    </div>
+                    <div className="flex items-center border border-gray-700 bg-transparent gap-2 px-3 py-1.5 rounded-full font-bold text-white text-sm shadow hover:bg-zinc-700 transition select-none"> 
+                      <TbBrandFramerMotion className="text-pink-400 text-xl" />
+                      Framer-Motion
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
